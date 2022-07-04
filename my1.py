c1b3e976ffc2eba2a440cc10b6adf973e180a719
thechampthapa@gmail.com <thechampthapa@gmail.com>
2022-07-04 12:20:15 +0530

    done

--- my1.py
+++ my1.py
@@ -1,4 +1,6 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
 print("Enter 1 for addition\n2 for subtraction\n3 for multiplication\n 4 for division:")
@@ -12,6 +14,36 @@ elif n==3:
     print("multiplication =",a*b)
 elif n==4:
     print("division =",a/b)
+=======
+a = int(input("Enter the First number:"))
+b = int(input("Enter the Second number:"))
+print("1. Addition\n 2. Subtraction\n3. Multiplication\n 4. Division:")
+
+n=int(input("Enter your choice:"))
+if n==1:
+    print("addition =",a+b)
+elif n==2:
+    print("subtraction =",a-b)
+elif n==3:
+    print("multiplication =",a*b)
+elif n==4:
+    print("division =",a/b)
+>>>>>>> Stashed changes
+=======
+a = int(input("Enter the First number:"))
+b = int(input("Enter the Second number:"))
+print("1. Addition\n 2. Subtraction\n3. Multiplication\n 4. Division:")
+
+n=int(input("Enter your choice:"))
+if n==1:
+    print("addition =",a+b)
+elif n==2:
+    print("subtraction =",a-b)
+elif n==3:
+    print("multiplication =",a*b)
+elif n==4:
+    print("division =",a/b)
+>>>>>>> Stashed changes
 =======
 a = int(input("Enter the First number:"))
 b = int(input("Enter the Second number:"))
